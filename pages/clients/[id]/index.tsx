c06ee438de2346314b2dd07b7891b80bc7b33a86
ph-gomes <rphdgomes@gmail.com>
2023-03-06 22:10:00 -0300

    programmatically navigation

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -4,9 +4,20 @@ export default function Client() {
   const router = useRouter();
   const { id } = router.query;
 
+  const loadProjectHandler = () => {
+    // do some stuff
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "foo", clientProjectId: "bar" },
+    });
+  };
+
   return (
     <div>
-      <h1>Client Id:&quot;{id}&quot;</h1>
+      <h1>The Projects of a Given Client</h1>
+      <h2>Client Id: &quot;{id}&quot;</h2>
+
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
