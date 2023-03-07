492e984a4b36b029e217f36259691ac82290942c
ph-gomes <rphdgomes@gmail.com>
2023-03-06 22:03:40 -0300

    working with links

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Portfolio() {
+  const projects = [
+    { id: "foo", name: "Foo" },
+    { id: "bar", name: "Bar" },
+  ];
+
   return (
     <div>
       <h1>Portfolio page</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <Link href={`/portfolio/${project.id}`}>{project.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
