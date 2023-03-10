8856c942a45c8af2f674e46ed575e94818607850
ph-gomes <rphdgomes@gmail.com>
2023-03-09 21:06:52 -0300

    feature: create event filter page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,13 +1,40 @@
+import { EventList } from "@/components/events/event-list";
+import { getFilteredEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
 
 export const FilteredEventsPage = () => {
   const router = useRouter();
-  const { slug } = router.query;
+  const { slug: filterData } = router.query as { slug: string[] };
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const [rawYear, rawMonth] = filterData;
+
+  const year = +rawYear;
+  const month = +rawMonth;
+
+  if (
+    isNaN(month) ||
+    isNaN(year) ||
+    year < 2021 ||
+    year > 2030 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return <p className="center">Invalid Filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year, month });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No events found for the chosen filter!</p>;
+  }
 
   return (
     <div>
-      <h1>Filtered Event</h1>
-      <p>Event SLUG: {(slug as string[]).join("/")}</p>
+      <EventList items={filteredEvents} />
     </div>
   );
 };
